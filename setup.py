a37adc9226e7b9730d9291d38c3358795ddad39a
Martin R Mortensen, KIT <martin.mortensen@adm.ku.dk>
2016-04-21 12:23:41 +0200

    Totally redone to include login form if client hasnt got kerberos ticket

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ Links
 from setuptools import setup
 
 setup(name='Bottle-Kerberos',
-      version='1.0.0',
+      version='1.1.0',
       url='http://github.com/martinrm77/bottle-kerberos',
       license='BSD',
       author='Michael Komitee, Martin Mortensen',
